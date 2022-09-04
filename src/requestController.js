6b241a0445a5182018e7435ddcb04bc5a1efa42d
Michael L Bangura <michlawbang123@gmail.com>
2022-09-04 01:42:36 -1200

    done

--- src/requestController.js
+++ src/requestController.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-const BASE_URL = "https://mern-app-blog-app.herokuapp.com/api/";
+const BASE_URL = "https://mern--blogging.herokuapp.com/api/";
 
 
 export const publicRequest = axios.create({
